b3dd9c321014cd0b280dcd33d4371aebfe51be62
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-12-12 18:38:35 +0100

    yeet

--- src/context/PickerContext.ts
+++
@@ -1,6 +0,0 @@
-import React, { Dispatch, SetStateAction } from 'react';
-import { PickerContextType } from '../hooks/usePickerContext';
-
-const PickerContext = React.createContext<[PickerContextType, Dispatch<SetStateAction<PickerContextType>>]>(null!);
-
-export default PickerContext;
--- src/context/SettingsContext.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { GifPickerSettings } from '../hooks/useSettings';
-
-const SettingsContext = React.createContext<GifPickerSettings>(null!);
-
-export default SettingsContext;
