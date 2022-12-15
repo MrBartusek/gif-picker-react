a11909f3403f18a1df66f858fa935745255f2a3c
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-12-15 16:51:14 +0100

    Add example for gh pages

---
+++ example/src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import GifPicker, { TenorImage } from 'gif-picker-react';
+import { useState } from 'react';
+import './styles.css';
+
+const TENOR_API_KEY = 'AIzaSyBh5BTijm12gMpzUYpyfbKN8pjszpUR1FE';
+
+function App(): JSX.Element {
+	const [ selected, setSelected ] = useState<TenorImage>(null!);
+
+	return (
+		<div className="App">
+			<h2>Gif Picker React Demo</h2>
+			<div className="show-gif">
+        Your selected GIF is:
+				{selected && (
+					<>
+						<img
+							src={selected.url}
+							className="gif-preview"
+							alt="Selected GIF"
+						/>
+						<a href={selected.shortTenorUrl} target="_blank" rel="noreferrer">
+							{selected.shortTenorUrl}
+						</a>
+					</>
+				)}
+			</div>
+
+			<GifPicker tenorApiKey={TENOR_API_KEY} onGifClick={setSelected} />
+
+			<div className='footer'>
+				<code>gif-picker-react</code> - {' '}
+				<a href="https://github.com/MrBartusek/gif-picker-react" target="_blank" rel="noreferrer">github</a> - {' '}
+				<a href="https://www.npmjs.com/package/gif-picker-react" target="_blank" rel="noreferrer">npm</a>
+			</div>
+		</div>
+	);
+}
+export default App;
