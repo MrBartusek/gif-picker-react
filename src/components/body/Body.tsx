9675836e21e83fda227e0ab170a61e55ad6ed5f3
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-05-15 20:16:40 +0200

    Dynamic columns

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import PickerContext from '../../context/PickerContext';
 import TenorContext from '../../context/TenorContext';
 import { TenorCategory } from '../../managers/TenorManager';
@@ -8,12 +8,27 @@ import CategoryList from './CategoryList';
 import SearchResult from './SearchResult';
 import TrendingResult from './TrendingResult';
 
-function Body(): JSX.Element {
+const MAX_COLUMN_WIDTH = 170;
+
+export interface BodyProps {
+	/**
+	 * Width prop is here is for sole purpose of updating the
+	 * columns calculations when width prop change
+	 */
+	width?: number | string;
+}
+
+function Body({ width }: BodyProps): JSX.Element {
 	const [ categories, setCategories ] = useState<TenorCategory[] | undefined>(undefined);
 	const [ trending, setTrending ] = useState<TenorImage | undefined>(undefined);
 	const [ pickerContext ] = useContext(PickerContext);
+	const [ columnsCount, setColumnsCount ] = useState(1);
 	const tenor = useContext(TenorContext);
+	const ref = useRef<HTMLDivElement>(null);
 
+	/**
+	 * Load categories and first trending image for home page
+	 */
 	useEffect(() => {
 		(async (): Promise<any> => {
 			const categoryList = await tenor.categories();
@@ -23,17 +38,27 @@ function Body(): JSX.Element {
 		})();
 	}, []);
 
+	/**
+	 * Calculate amount of columns to display
+	 */
+	useEffect(() => {
+		const width = ref.current ? ref.current.offsetWidth : 0;
+		let columns = Math.floor( width / MAX_COLUMN_WIDTH );
+		if(columns < 1) columns = 1;
+		setColumnsCount(columns);
+	}, [ ref.current, width ]);
+
 	return (
-		<div className='gpr-body'>
+		<div className='gpr-body' ref={ref}>
 			{((): JSX.Element => {
 				if(pickerContext.showTrending) {
-					return <TrendingResult />;
+					return <TrendingResult columnsCount={columnsCount} />;
 				}
 				else if(pickerContext.searchTerm) {
-					return <SearchResult searchTerm={pickerContext.searchTerm} />;
+					return <SearchResult columnsCount={columnsCount} searchTerm={pickerContext.searchTerm} />;
 				}
 				else {
-					return <CategoryList categories={categories} trending={trending} />;
+					return <CategoryList columnsCount={columnsCount} categories={categories} trending={trending} />;
 				}
 			})()}
 		</div>
--- src/components/body/CategoryList.tsx
+++ src/components/body/CategoryList.tsx
@@ -9,21 +9,28 @@ import TrendingCategory from './TrendingCategory';
 export interface CategoryListProps {
 	categories?: TenorCategory[];
 	trending?: TenorImage;
+	columnsCount: number;
 }
 
-function CategoryList({ categories, trending }: CategoryListProps): JSX.Element {
+function CategoryList({ categories, trending, columnsCount }: CategoryListProps): JSX.Element {
+	// Reduce categories to multiple of columns so there won't by any not full rows at the bottom
+	let categoriesSliced = categories;
+	if(columnsCount > 1) {
+		categoriesSliced = categories?.slice(0, -((categories.length + 1) % columnsCount));
+	}
+
 	return (
-		<div className='gpr-category-list'>
+		<div className='gpr-category-list' style={{gridTemplateColumns: `repeat(${columnsCount}, 1fr)`}}>
 			{categories && trending ? (
 				<>
 					<TrendingCategory image={trending.url} />
-					{categories.map((cat, i) => (
+					{categoriesSliced?.map((cat, i) => (
 						<FeaturedCategory key={i} image={cat.image} name={cat.name} />
 					))}
 				</>
 			) : (
 				<>
-					{[ ...Array(10) ].map((_, i) => (
+					{[ ...Array(10 * columnsCount) ].map((_, i) => (
 						<CategoryPlaceholder key={i} />
 					))}
 				</>
