2265deac79504f42424b04518e1633b2ad967a70
Bartosz Dokurno <bartusekcraft@gmail.com>
2026-05-21 23:50:58 +0200

    Migrate from tsup to tsdown (#45)

---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+	entry: ['src/index.tsx'],
+	treeshake: true,
+	sourcemap: true,
+	minify: true,
+	clean: true,
+	dts: true,
+	format: ['cjs', 'esm'],
+	css: { inject: true },
+});
