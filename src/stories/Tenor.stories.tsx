23cea4c7e5fa976c9616641418d25415d2ae0b58
MrBartusek <bartusekcraft@gmail.com>
2026-06-01 22:18:24 +0200

    Run lint

--- src/stories/Tenor.stories.tsx
+++ src/stories/Tenor.stories.tsx
@@ -3,24 +3,21 @@ import { createProviderMeta } from './providerStory';
 
 export default {
 	title: 'Providers/Tenor',
-	...createProviderMeta(
-		(args) => Tenor(process.env.STORYBOOK_TENOR_TOKEN!, args),
-		{
-			contentFilter: {
-				options: Object.values(ContentFilter),
-				control: { type: 'select' },
-				table: { category: 'Tenor' },
-			},
-			country: {
-				control: { type: 'text' },
-				table: { category: 'Tenor' },
-			},
-			locale: {
-				control: { type: 'text' },
-				table: { category: 'Tenor' },
-			},
+	...createProviderMeta((args) => Tenor(process.env.STORYBOOK_TENOR_TOKEN!, args), {
+		contentFilter: {
+			options: Object.values(ContentFilter),
+			control: { type: 'select' },
+			table: { category: 'Tenor' },
 		},
-	),
+		country: {
+			control: { type: 'text' },
+			table: { category: 'Tenor' },
+		},
+		locale: {
+			control: { type: 'text' },
+			table: { category: 'Tenor' },
+		},
+	}),
 	args: {
 		contentFilter: ContentFilter.OFF,
 	},
