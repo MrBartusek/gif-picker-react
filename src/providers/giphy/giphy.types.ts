4e5be63214f6a40352148c8926a805f8e52e44c7
MrBartusek <bartusekcraft@gmail.com>
2026-06-06 20:07:02 +0200

    Add missing types to reponse

--- src/providers/giphy/giphy.types.ts
+++ src/providers/giphy/giphy.types.ts
@@ -11,16 +11,20 @@ export interface GiphyMediaFormat {
 	height: string;
 	size?: string;
 	mp4?: string;
+	mp4_size?: string;
 	webp?: string;
+	webp_size?: string;
+	frames?: string;
+	hash?: string;
 }
 
 export interface GiphyImages {
-	original: GiphyMediaFormat;
-	fixed_width: GiphyMediaFormat;
-	fixed_height: GiphyMediaFormat;
-	downsized: GiphyMediaFormat;
-	preview_gif: GiphyMediaFormat;
-	[key: string]: GiphyMediaFormat;
+	original?: GiphyMediaFormat;
+	fixed_width?: GiphyMediaFormat;
+	fixed_height?: GiphyMediaFormat;
+	downsized?: GiphyMediaFormat;
+	preview_gif?: GiphyMediaFormat;
+	[key: string]: GiphyMediaFormat | undefined;
 }
 
 export interface GiphyAnalyticsEvent {
@@ -38,13 +42,22 @@ export interface GiphyGif {
 	id: string;
 	slug: string;
 	url: string;
+	bitly_url: string;
+	bitly_gif_url: string;
+	embed_url: string;
+	content_url: string;
 	title: string;
-	rating: string;
+	rating: ContentRating;
 	username: string;
-	source?: string;
+	source: string;
+	source_tld: string;
+	source_post_url: string;
+	is_sticker: number;
+	import_datetime: string;
+	trending_datetime: string;
 	user?: any;
 	images: GiphyImages;
-	analytics: GiphyAnalytics;
+	analytics?: GiphyAnalytics;
 	analytics_response_payload?: string;
 }
 
@@ -69,7 +82,7 @@ export interface GiphyListResponse {
 export interface GiphyCategory {
 	name: string;
 	name_encoded: string;
-	gif: GiphyGif;
+	gif?: GiphyGif;
 	subcategories: any[];
 }
 
