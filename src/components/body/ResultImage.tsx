97f56a38b39b15c1b80918748f53e285d55504ce
MrBartusek <bartusekcraft@gmail.com>
2026-06-06 18:15:22 +0200

    Better error handling for onclick

--- src/components/body/ResultImage.tsx
+++ src/components/body/ResultImage.tsx
@@ -15,15 +15,15 @@ function ResultImage({ gif, searchTerm }: ResultImageProps): React.JSX.Element {
 
 	async function onClick(): Promise<void> {
 		try {
-			const func = settings.onGifClick;
-
-			if (func) {
-				await func(gif);
-			}
-
-			await provider.onClick?.(gif, { searchTerm });
+			await settings.onGifClick?.(gif);
 		} catch (error) {
 			console.error('[gif-picker-react] Failed to handle GIF selection', error);
+		}
+
+		try {
+			await provider.onClick?.(gif, { searchTerm });
+		} catch (error) {
+			console.error('[gif-picker-react] Failed to register GIF click', error);
 		}
 	}
 
